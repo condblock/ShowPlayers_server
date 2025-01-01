ce792ac48892b84a1276c15c668b703bddaf44d6
COND_B <72214833+condblock@users.noreply.github.com>
2025-01-01 17:55:50 +0900

    edit port

--- main.py
+++ main.py
@@ -76,4 +76,4 @@ def form():
     return render_template('form.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=5000)
